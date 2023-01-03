046e690d6dd79d7bbda2bbd6e0f9fd5652ccd876
Ramiz Ali <ramiz.apk7@gmail.com>
2023-01-03 06:21:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(arr,name){
+    const result = arr.filter(arr => arr.toLowerCase().includes(name.toLowerCase()));
+    return result;
+}
+
+function fuzzyMatch(arr,letter){
+    const result = arr.filter(arr => arr.startsWith(letter));
+    return result;
+}
+
+function matchName(obj,name){
+    const result = obj.filter(obj => obj.name.includes(name));
+    return result;
+}
